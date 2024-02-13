8f007a651baafd6ad49e4c33c044219151c36e06
lorencjakub <69380964+lorencjakub@users.noreply.github.com>
2024-02-13 08:23:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def index():
-    return jsonify({"Choo Choo": "Token"})
+    return jsonify({"Choo Choo": "Choo Choo Token"})
 
 
 if __name__ == '__main__':
